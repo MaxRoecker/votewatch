b61c3bc4c2c4e8f0f682e2b9c8f16e2e4ef46e79
Max Naegeler Roecker <max.roecker@gmail.com>
2024-02-12 09:45:38 -0300

    Update homepage

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,56 +1,69 @@
-import type { MetaFunction } from '@remix-run/node';
-import { Link } from '@remix-run/react';
+import { Link, json, useLoaderData } from '@remix-run/react';
+import { BillCard } from '~/bills/components/bill-card';
+import { listBills } from '~/bills/data';
+import { Button } from '~/commons/components/button';
+import { useOutletContext } from '~/commons/utils/context';
+import { getIntl } from '~/commons/utils/intl';
+import { LegislatorCard } from '~/legislators/components/legislator-card';
+import { listLegislators, retrieveLegislator } from '~/legislators/data';
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: 'New Remix App' },
-    { name: 'description', content: 'Welcome to Remix!' },
-  ];
+export const loader = async () => {
+  const [legislators, bills] = await Promise.all([
+    listLegislators({ offset: 0, limit: 4 }),
+    listBills({ offset: 0, limit: 4 }),
+  ]);
+  const sponsors = await Promise.all(
+    bills.map((bill) => retrieveLegislator(bill.sponsorId)),
+  );
+  return json({ legislators, bills, sponsors });
 };
 
-export default function Index() {
-  return (
-    <div className="mx-auto my-0 flex max-w-screen-xl flex-col gap-4 p-4 md:p-6">
-      <header className="flex items-center justify-between">
-        <h1 className="scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-4xl">
-          <Link to="/">VoteWatch</Link>
-        </h1>
-        {/* <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button className="md:hidden" icon>
-              <HamburgerMenuIcon />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Menu</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem asChild>
-              <Link to="..">
-                <FormattedMessage id="Properties" defaultMessage="Properties" />
-              </Link>
-            </DropdownMenuItem>
-            <DropdownMenuItem asChild>
-              <Link to="bookings">
-                <FormattedMessage id="Bookings" defaultMessage="Bookings" />
-              </Link>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+export type Loader = typeof loader;
 
-        <nav className="hidden flex-row items-center justify-stretch md:flex">
-          <Button variant="link" data-testid="nav-properties" asChild>
-            <Link to="..">
-              <FormattedMessage id="Properties" defaultMessage="Properties" />
-            </Link>
-          </Button>
-          <Separator orientation="vertical" />
-          <Button variant="link" data-testid="nav-bookings" asChild>
-            <Link to="bookings">
-              <FormattedMessage id="Bookings" defaultMessage="Bookings" />
-            </Link>
-          </Button>
-        </nav> */}
-      </header>
+export default function Index() {
+  const { legislators, bills, sponsors } = useLoaderData<Loader>();
+  const { locale } = useOutletContext();
+  const intl = getIntl(locale);
+  return (
+    <div className="grid grid-cols-1 gap-12 md:grid-cols-2 md:gap-6">
+      <section className="flex flex-col gap-4 md:gap-6">
+        <h2 className="scroll-m-20 pb-2 text-2xl font-semibold tracking-tight first:mt-0">
+          {intl.formatMessage({ id: 'Latest Legislators' })}
+        </h2>
+        <div className="flex flex-col gap-4 md:gap-6" role="list">
+          {legislators.map((legislator) => {
+            return (
+              <Link key={legislator.id} to={`/legislators/${legislator.id}`}>
+                <LegislatorCard role="listitem" legislator={legislator} />
+              </Link>
+            );
+          })}
+        </div>
+        <Button asChild>
+          <Link to="/legislators">
+            {intl.formatMessage({ id: 'See all legislators' })}
+          </Link>
+        </Button>
+      </section>
+      <section className="flex flex-col gap-4 md:gap-6">
+        <h2 className="scroll-m-20 pb-2 text-2xl font-semibold tracking-tight first:mt-0">
+          {intl.formatMessage({ id: 'Latest Bills' })}
+        </h2>
+        <div className="flex flex-col gap-4 md:gap-6" role="list">
+          {bills.map((bill, index) => {
+            const sponsor = sponsors.at(index);
+            return (
+              <Link key={bill.id} to={`/bills/${bill.id}`}>
+                <BillCard role="listitem" bill={bill} sponsor={sponsor} />
+              </Link>
+            );
+          })}
+        </div>
+
+        <Button asChild>
+          <Link to="/bills">{intl.formatMessage({ id: 'See all bills' })}</Link>
+        </Button>
+      </section>
     </div>
   );
 }
